adb91cc0c233cec49219daf683d81011e389e92e
zwug <kirill913@mail.ru>
2014-05-06 23:58:42 +0400

    Fork dup2 execlp lab for popen

--- lab_popen/main.cpp
+++ lab_popen/main.cpp
@@ -2,41 +2,40 @@
 #include <iostream>
 #include <stdio.h>
 #include <unistd.h>
-
+#include <string.h>
 using namespace std;
 
 int main() {
-int p[2];//pipe
-int status;
-char buff[512];
-switch(fork()){
-case -1:
-    perror("Fork error");
-case 0:
-    if(pipe(p) == -1)
-        perror("Pipe error");
-    printf("Child");
-            close(p[0]);
-        close(p[1]);
-    if(fork() == 0){
-        close(p[0]);
-        close(p[1]);
-        execlp( "/bin/ls", "ls", "-R", NULL );
+int p[2]; //pipe
+if(pipe(p) == -1)
+    perror("Pipe error");
+for(;;){
+    char command[10], *comm, argument[20];
+    cout<<"Type your command\n";
+    cin>>command;
+    cout<<"Type argument\n";
+    cin>>argument;
+    char path[] = "/bin/";
+    comm = strcat(path, command);
+    cout<<comm;
+    switch(fork()){
+    case -1:
+        perror("Fork error");
+    case 0:
+        close(1);
+        dup2(p[1], 1);
+        if(strcmp(argument, "null") == 0)
+            execlp(path , command, NULL, NULL);
+        execlp(path , command, argument, NULL);
+        perror("Execl error");
+        return 0;
+    default:
+        char buff[512] = "";
+        if(read(p[0], buff, 512)>0){
+            printf("MSG=%s\n",buff);
+        }
+        printf("\n----------Done---------\n");
     }
-    //dup(p[1]);
-    printf("Hello22222222222");
-
-    printf("Hello333333333333333");
-    perror("Execl error");
-default:
-    wait(&status);
-    printf("parernt");
-    //dup2(p[0], 0);
-//    if(read(p[0], buff, 256)){
-        //cout<<buff;
-   // }
-    //return(status);
 }
-
 return 0;
 }
