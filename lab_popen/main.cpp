d1fcf6ee1257c9197dcd7ce797b4ad988a064fd7
zwug <kirill913@mail.ru>
2014-05-06 17:57:02 +0400

    Update main.cpp
    govnocod

--- lab_popen/main.cpp
+++ lab_popen/main.cpp
@@ -1,3 +1,4 @@
+#include <sys/wait.h>
 #include <iostream>
 #include <stdio.h>
 #include <unistd.h>
@@ -5,18 +6,37 @@
 using namespace std;
 
 int main() {
-	FILE *in;
-	char buff[512];
-	execl("/bin/ls", "ls",(char *)0);
-execl("ls", "ls", 0);
-	if(!(in = popen("touch 2.txt", "w"))){
-		return 1;
-	}
+int p[2];//pipe
+int status;
+char buff[512];
+switch(fork()){
+case -1:
+    perror("Fork error");
+case 0:
+    if(pipe(p) == -1)
+        perror("Pipe error");
+    printf("Child");
+            close(p[0]);
+        close(p[1]);
+    if(fork() == 0){
+        close(p[0]);
+        close(p[1]);
+        execlp( "/bin/ls", "ls", "-R", NULL );
+    }
+    //dup(p[1]);
+    printf("Hello22222222222");
 
-	while(fgets(buff, sizeof(buff), in)!=NULL){
-		cout << buff;
-	}
-	pclose(in);
+    printf("Hello333333333333333");
+    perror("Execl error");
+default:
+    wait(&status);
+    printf("parernt");
+    //dup2(p[0], 0);
+//    if(read(p[0], buff, 256)){
+        //cout<<buff;
+   // }
+    //return(status);
+}
 
-	return 0;
+return 0;
 }
